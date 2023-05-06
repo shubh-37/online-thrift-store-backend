c40301d23483fab5c95957af7ee6360d04c9efd5
shubh-37 <shubh.arya37@gmail.com>
2023-05-07 01:31:13 +0530

    model without image field

---
+++ models/product.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+    prodName: {
+        type: String,
+        unique: [true, "Product already exists"],
+        required: [true, "Product name required"],
+        maxLength: [15, "Too long"],
+    },
+    prodSize: {
+        type: String,
+        enum: ["XS","S","M","L","XL"],
+        default: "S",
+        required: [true, "Please provide a size"]
+    },
+    quantity: {
+        type: Number,
+        default: 1,
+        required: [true, "Product quantity is required"]
+    },
+    availability: {
+        type: Boolean,
+        default: false
+    },
+    prodPrice: {
+        type: Number,
+        required: [true, "Product price is necessary"],
+    }
+});
+
+module.exports = productSchema;
