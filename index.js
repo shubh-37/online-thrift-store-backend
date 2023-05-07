2500d8fad5bed06c1f55d1cce2480ada09f06b50
shubh-37 <shubh.arya37@gmail.com>
2023-05-07 18:34:38 +0530

    added middlewares and made connection with database

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
+const connectDB = require("./connect");
+const cors = require("cors");
+
+//middlewares
+app.use(express.json())
+app.use(cors());
 
 app.get("/", (req,res) => {
     res.send("Hello world!")
 });
 
-app.listen(3000,()=> {
-    console.log("Server listening at port: 3000")
-})
+try {
+    async function start(){
+        const { Models } = await connectDB();
+        app.listen(3000, console.log("Server listening on port: 3000"));
+    }
+    start();
+} catch (error) {
+    console.log("Sorry! cannot start the server", error);
+}
