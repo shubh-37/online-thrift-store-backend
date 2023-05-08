a3cc1f429ecbcb235985b147aca16e8d2a16ac9d
shubh-37 <shubh.arya37@gmail.com>
2023-05-08 13:41:59 +0530

    API for creating products

---
+++ controllers/products.js
@@ -0,0 +1,23 @@
+
+function product(app, Models){
+    const { Product } = Models;
+    app.post("/createProd", async function createProd(req,res){
+        const { prodName, prodSize, quantity, availability, prodPrice } = req.body;
+        try {
+            const prod = await Product.create({
+                prodName,
+                prodSize,
+                quantity,
+                availability,
+                prodPrice
+            });
+            if (prod) {
+                return res.status(201).json({ _id: prod._id, name: prod.prodName, message: "Product created"});
+            }else{
+                return res.status(404).json({message: "Couldn't create product! Please try again"})
+            }
+        } catch (error) {
+                return res.status(500).json({message: error.message})
+        }
+    });
+}
