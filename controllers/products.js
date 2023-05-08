677499fbe7d42417ef9618b454258cf20fe2a951
shubh-37 <shubh.arya37@gmail.com>
2023-05-09 02:42:32 +0530

    delete prod API and added user field

--- controllers/products.js
+++ controllers/products.js
@@ -2,14 +2,15 @@
 function products(app, Models){
     const { Product } = Models;
     app.post("/createProd", async function createProd(req,res){
-        const { prodName, prodSize, quantity, availability, prodPrice } = req.body;
+        const { prodName, prodSize, quantity, availability, prodPrice, user } = req.body;
         try {
             const prod = await Product.create({
                 prodName,
                 prodSize,
                 quantity,
                 availability,
-                prodPrice
+                prodPrice,
+                user
             });
             if (prod) {
                 return res.status(201).json({ _id: prod._id, name: prod.prodName, message: "Product created"});
@@ -20,6 +21,20 @@ function products(app, Models){
                 return res.status(500).json({message: error.message})
         }
     });
+    app.delete("/deleteProd/:userId", async function deleteProd(req, res){
+        const { userId } = req.params;
+        const { prodName } = req.body;
+        try {
+            const deletedProd = await Product.findOneAndDelete({ user: userId, prodName: prodName });
+            if(deletedProd){
+                return res.status(200).json({message: "product deleted successfully", name: deletedProd.prodName});
+            }else{
+                return res.status(404).json({message: "product not found"});
+            }
+        } catch (error) {
+            return res.status(500).json({message: error.message});
+        }
+    })
 }
 
 module.exports = products;
