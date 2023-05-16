80faa09c3af0990ad02a87993efaf226a161275d
shubh-37 <shubh.arya37@gmail.com>
2023-05-17 00:17:42 +0530

    refactored the try block

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -1,19 +1,18 @@
 const jwt = require("jsonwebtoken");
 
 const auth = (req, res, next) => {
+  try {
   const authHeader = req.headers.authorization;
   if(!authHeader || authHeader.startsWith("Bearer ")){
     throw new Error("Authentication Invalid!");
   }
-
   const token = authHeader.split(" ")[1];
-
-  try {
+  
     const payload = jwt.verify(token, process.env.JWT_SECRET);
     req.user = {userId: payload.userID};
     next();
   } catch (error) {
-    console.error(error);
+    return res.status(401).send({message: error.message});
   }
 }
 
